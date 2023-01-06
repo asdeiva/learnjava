170190cadb0fa77a2f0ab1e463742a4721325211
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-01-06 15:22:44 +0530

    avg calc prog Added

---
+++ Basic_Java/AverageCalc.java
@@ -0,0 +1,52 @@
+/*
+Average Calculator
+Given the marks of n students, your task is to take input of marks, calculate the average of the marks obtained and print it.
+
+Input Format
+The first number in the input line is the number of students.
+
+The second line contains n space-separated integers are the marks of the students.
+
+Output Format
+Print the floor of the average of marks obtained.
+
+Example 1
+Input
+
+5
+2 2 2 2 2
+Output
+
+2
+Example 2
+Input
+
+3
+31 32 33
+Output
+
+32
+Constraints
+1 <= n <= 10000
+1 <= marks <= 10000
+*/
+import java.util.*;
+import java.io.*;
+
+public class AverageCalc
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int nums[]=new int [n];
+      int sum=0;
+      int avg=0;
+      for (int i=0;i<n;i++){
+        nums[i]=sc.nextInt();
+        sum+=nums[i];
+      }
+      avg=sum/n;
+      System.out.println(avg);
+	}
+}
